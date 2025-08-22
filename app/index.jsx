2611b4b680880bdaf602cf5b4f3a2d9a9894592e
staceyanneabellana883-hue <staceyanneabellana883@gmail.com>
2025-08-22 16:51:55 +0800

    color was changed

--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#3984d0ff',
     color: 'white',
     borderRadius: 8,
   },
